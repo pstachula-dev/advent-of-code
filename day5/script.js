82665e81b9d63bf85a435884397cd97b84569db4
pawel stachula <pawel.stachula@lastminute.com>
2021-05-18 23:34:21 +0200

    day5 improvements

--- day5/script.js
+++ day5/script.js
@@ -1,6 +1,6 @@
 import { readInput } from "../utils.js";
 
-const data = (await readInput('./input.js', Infinity));
+const data = (await readInput('./input.js'));
 
 const maxCol = 128;
 const maxRow = 8;
@@ -16,12 +16,9 @@ const getSeatIndex = ({ list, maxNum, isLower }) => {
 
     for (const el of list) {
         index = index / 2;
-
-        if (el === isLower) {
-            seatRight = seatRight - index;
-        } else if (index) {
-            seatLeft = seatLeft + index;
-        }
+        el === isLower
+            ? seatRight -= index
+            : seatLeft += index;
 
         if (index === 1) {
             return el === isLower ? seatLeft : seatRight - 1;
@@ -31,17 +28,14 @@ const getSeatIndex = ({ list, maxNum, isLower }) => {
 
 const part1 = ({ data, maxCol, maxRow }) => {
     return Math.max(...data.map(row => {
-        const bfStr = row.substr(0, 7);
-        const rlStr = row.substr(7, 3);
-
         const colIndex = getSeatIndex({
-            list: [...bfStr],
+            list: [...row.substr(0, 7)],
             maxNum: maxCol,
             isLower: ZONES.F,
         });
 
         const rowIndex = getSeatIndex({
-            list: [...rlStr],
+            list: [...row.substr(7, 3)],
             maxNum: maxRow,
             isLower: ZONES.L,
         });
