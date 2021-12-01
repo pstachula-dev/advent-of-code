4ce049c1a1ca48fc1554c1d7005a7b0282dad46b
pstachula <pawel.stachula.mail@gmail.com>
2021-12-01 20:49:21 +0100

    feat: add day1/p2 2021

--- 2021/day1/index.js
+++ 2021/day1/index.js
@@ -4,8 +4,17 @@ const data = (await readInput("./2021/day1/input.js")).map((e) => parseInt(e));
 
 // P1
 const part1 = (data) => data.filter((el, index) => el < data[index + 1]).length;
+
 console.log("P1", part1(data));
 
 // P2
-const part2 = (data) => data.filter((el, index) => el < data[index + 1]).length;
+const sumArray = (data, index, movePointer = 0) =>
+  data
+    .slice(index + movePointer, index + 3 + movePointer)
+    .reduce((a, b) => a + b, 0);
+
+const part2 = (data) => {
+  return data.filter((_, i) => sumArray(data, i) < sumArray(data, i, 1)).length;
+};
+
 console.log("P2", part2(data));
