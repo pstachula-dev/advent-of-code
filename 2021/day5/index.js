97e1a7b2d1b6fcb50858eb71a212e6e189c9b715
pawel <pawel.stachula.mail@gmail.com>
2021-12-10 23:45:36 +0100

    feat: day5 p2 try

--- 2021/day5/index.js
+++ 2021/day5/index.js
@@ -20,7 +20,7 @@ const parsePoints = (data) =>
       });
   });
 
-const matrix = createMatrix(1000);
+const matrix1 = createMatrix(10);
 const data = parsePoints(text);
 
 // P1
@@ -28,25 +28,51 @@ const data = parsePoints(text);
 const part1 = ({ data, matrix }) => {
   for (const [point1, point2] of data) {
     if (point1.x === point2.x) {
-      const min = Math.min(point1.y, point2.y);
-      const max = Math.max(point1.y, point2.y);
-      for (let i = min; i <= max; i++) matrix[point1.x][i]++;
+      const [min, max] = [point1.y, point2.y].sort();
+      for (let i = min; i <= max; i++) matrix[i][point1.x]++;
     }
 
     if (point1.y === point2.y) {
-      const min = Math.min(point1.x, point2.x);
-      const max = Math.max(point1.x, point2.x);
-      for (let i = min; i <= max; i++) matrix[i][point1.y]++;
+      const [min, max] = [point1.x, point2.x].sort();
+      for (let i = min; i <= max; i++) matrix[point1.y][i]++;
     }
   }
+  return matrix.flat().filter((el) => el >= 2).length;
+};
+
+console.log("P1", part1({ data, matrix: matrix1 }));
+
+// P2 FIXME: ...
+// ------------------------------------------------------------------------------
+const part2 = ({ data, matrix }) => {
+  for (const [point1, point2] of data) {
+    if (point1.x === point2.x) {
+      const [min, max] = [point1.y, point2.y].sort();
+      // for (let i = min; i <= max; i++) matrix[i][point1.x]++;
+    }
+
+    if (point1.y === point2.y) {
+      const [min, max] = [point1.x, point2.x].sort();
+      // for (let i = min; i <= max; i++) matrix[point1.y][i]++;
+    }
+
+    if (point1.y !== point2.y || point1.x !== point2.x) {
+      console.log(point1, point2);
+
+      for (let i = minY; i <= maxY; i++) {
+        for (let j = minX; j <= maxX; j++) {
+          if (i === j) {
+            matrix[i][j]++;
+          }
+        }
+      }
+    }
+  }
+
+  console.log(matrix.map((el) => el.join()));
 
   return matrix.flat().filter((el) => el >= 2).length;
 };
 
-console.log("P1", part1({ data, matrix }));
-
-// P2
-// ------------------------------------------------------------------------------
-const part2 = (data) => {};
-
-console.log("P2", part2(data));
+const matrix2 = createMatrix(10);
+console.log("P2", part2({ data, matrix: matrix2 }));
