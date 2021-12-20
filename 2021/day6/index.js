9926edc5d85d1a30197bd46bc03d9f8ae1edfb4d
pawel <pawel.stachula.mail@gmail.com>
2021-12-20 22:45:19 +0100

    feat: refactoring day6

--- 2021/day6/index.js
+++ 2021/day6/index.js
@@ -5,21 +5,23 @@ const data = input[0].split(",").map((e) => parseInt(e));
 
 const MIN_STATE = 0;
 const MAX_STATE = 6;
-const NEW_STATE = 8;
-const DAYS = 80;
+const NEW_STATE = 9;
+const DAYS = 18;
 
 // P1
 const part1 = (initData) => {
-  let result = initData;
+  let result = new Map(Object.entries(initData));
 
   for (let i = 0; i < DAYS; i++) {
-    result = [
-      ...result.map((el) => (el > MIN_STATE ? el - 1 : MAX_STATE)),
-      ...result.filter((e) => e === MIN_STATE).map((e) => NEW_STATE),
-    ];
+    result.forEach((el, index) => {
+      if (el === MIN_STATE) {
+        result.set(result.size, NEW_STATE);
+      }
+      result.set(index, el > MIN_STATE ? el - 1 : MAX_STATE);
+    });
   }
 
-  return result.length;
+  return result.size;
 };
 
 console.log("P1", part1(data));
