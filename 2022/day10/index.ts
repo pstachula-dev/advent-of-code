b99b0300a7ae82ee124e82515c105a1946d94585
pstachula <pawel.stachula.mail@gmail.com>
2022-12-12 23:31:40 +0100

    rename

--- 2022/day10/index.ts
+++ 2022/day10/index.ts
@@ -45,8 +45,6 @@ runner({
       }
     }
 
-    console.log('expected', result.slice(0, 6));
-
     return result
       .slice(0, 6)
       .map((el, index) => el * (20 + index * 40))
@@ -58,10 +56,48 @@ runner({
 runner({
   path,
   solution: (input) => {
-    const data = splitIntoGroups({
-      input,
+    let cyclesTotal = 0;
+    const screen: string[][] = [];
+
+    const data = splitToFlatArray({ input }).map((el) => {
+      const [register, value] = el.split(' ');
+
+      return {
+        register: register as REGISTER,
+        value: parseInt(value),
+      };
     });
 
+    const setSignal = (
+      cyclesTotal: number,
+      line: string[],
+      screen: string[][],
+      sprite: string[],
+    ) => {
+      cyclesTotal;
+
+      // if ((cyclesTotal - 20) % 40 === 0) {
+      // }
+    };
+
+    const sprite: string[] = ['#', '#', '#'];
+    const line: string[] = [];
+
+    for (let index = 0; index < data.length; index++) {
+      const el = data[index];
+      setSignal(cyclesTotal, line, screen, sprite);
+
+      if (el.register === 'noop') cyclesTotal += 1;
+
+      if (el.register === 'addx') {
+        for (let j = 0; j < 2; j++) {
+          cyclesTotal += 1;
+          setSignal(cyclesTotal, line, screen, sprite);
+        }
+      }
+    }
+
+    console.log(screen.map((line) => line.join('')).join('\n'));
     return 0;
   },
 });
