ef170ad86d049286fe444c9e4057f1b84fef1381
pstachula <pawel.stachula.mail@gmail.com>
2022-12-17 23:54:22 +0100

    refactor

--- 2022/day11/index.ts
+++ 2022/day11/index.ts
@@ -19,42 +19,30 @@ const parseMonkey = (row: string[]) => {
 };
 
 const monkeysInspector = (input: string, limit: number, divider?: number) => {
-  const monkeysInspected: Record<string, number> = {
-    '0': 0,
-    '1': 0,
-    '2': 0,
-    '3': 0,
-    '4': 0,
-    '5': 0,
-    '6': 0,
-    '7': 0,
-  };
+  const monkeysInspected: Record<string, number> = {};
   const data = splitIntoGroups({ input }).map(parseMonkey);
   const totalModulo = data.map(({ test }) => test).reduce((a, b) => a * b, 1);
 
   for (let i = 0; i < limit; i++) {
     data.map(({ monkey, items, op, test, ifTrue, ifFalse }, index) => {
+      const mStr = monkey.toString();
+      const operation = op[1];
+      const val1 = op[0];
+      const val2 = op[2];
+
       items.map((val) => {
-        monkeysInspected[monkey.toString()]++;
-        const oldVal2 = op[2] === 'old' ? val : parseInt(op[2], 10);
-        const oldVal1 = op[0] === 'old' ? val : parseInt(op[0], 10);
-        let monkeyIndex = 0;
-        let wLevel = 0;
+        monkeysInspected[mStr]
+          ? monkeysInspected[mStr]++
+          : (monkeysInspected[mStr] = 1);
+        const oldVal1 = val1 === 'old' ? val : parseInt(val1);
+        const oldVal2 = val2 === 'old' ? val : parseInt(val2);
+        const oldVal =
+          operation === '+' ? oldVal1 + oldVal2 : oldVal1 * oldVal2;
+        const wLevel = divider
+          ? Math.floor(oldVal / divider)
+          : (oldVal1 + oldVal2) % totalModulo;
 
-        if (op[1] === '+') {
-          wLevel = divider
-            ? Math.floor((oldVal1 + oldVal2) / divider)
-            : (oldVal1 + oldVal2) % totalModulo;
-        }
-
-        if (op[1] === '*') {
-          wLevel = divider
-            ? Math.floor((oldVal1 * oldVal2) / divider)
-            : (oldVal1 * oldVal2) % totalModulo;
-        }
-
-        monkeyIndex = wLevel % test === 0 ? ifTrue : ifFalse;
-        data[monkeyIndex].items.push(wLevel);
+        data[wLevel % test === 0 ? ifTrue : ifFalse].items.push(wLevel);
       });
       data[index].items = [];
     });
@@ -69,15 +57,11 @@ const monkeysInspector = (input: string, limit: number, divider?: number) => {
 // Part 1
 runner({
   path,
-  solution: (input) => {
-    return monkeysInspector(input, 20, 3);
-  },
+  solution: (input) => monkeysInspector(input, 20, 3),
 });
 
 // Part 2
 runner({
   path,
-  solution: (input) => {
-    return monkeysInspector(input, 10_000);
-  },
+  solution: (input) => monkeysInspector(input, 10_000),
 });
--- 2022/day12/index.ts
+++ 2022/day12/index.ts
@@ -1,4 +1,9 @@
-import { INPUT_PATH, runner, splitIntoGroups } from '../../lib/utils';
+import {
+  INPUT_PATH,
+  runner,
+  splitIntoGroups,
+  splitToFlatArray,
+} from '../../lib/utils';
 
 const path = `${__dirname}/${INPUT_PATH}`;
 
@@ -6,10 +11,8 @@ const path = `${__dirname}/${INPUT_PATH}`;
 runner({
   path,
   solution: (input) => {
-    const data = splitIntoGroups({
-      input,
-      parser: (e) => Number(e),
-    });
+    const data = splitToFlatArray({ input }).map((row) => row.split(''));
+    console.log(data);
 
     return 0;
   },
