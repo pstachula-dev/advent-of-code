feb0dc9f6d5eeed21c89c2e91952759604866849
pstachula-dev <pawel.stachula.mail@gmail.com>
2025-12-01 21:24:44 +0100

    feat: add day1

--- 2025/ts/day01/index.ts
+++ 2025/ts/day01/index.ts
@@ -5,11 +5,33 @@ import {
   splitLines,
 } from '../../../lib/utils';
 
-const path = `${__dirname}/${SAMPLE_PATH}`;
+const path = `${__dirname}/${INPUT_PATH}`;
 
 const solution = (input: string) => {
-  const lines = splitLines(input);
-  return lines;
+  let start = 50;
+  let counter = 0;
+
+  splitLines(input).forEach((el) => {
+    const [dir, ...str] = el.split('');
+    const num = parseInt(str.join(''));
+    const rest = num % 100;
+    const rotations = Math.floor(num / 100);
+    const prev = start;
+
+    start = dir === 'L' ? start - rest : start + rest;
+
+    // Part2
+    counter += rotations;
+    if (prev !== 0 && (start < 1 || start > 99)) counter++;
+
+    if (start < 0) start = 100 + start;
+    if (start > 99) start = Math.abs(100 - start);
+
+    // Part1
+    // if (start === 0) counter++;
+  });
+
+  return counter;
 };
 
 runner({
